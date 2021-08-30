c333161e6a60ef668bb804dcbc77e86f2a9926f9
sowmyalaveti-500apps <sowmya.laveti@500apps.com>
2021-08-30 18:16:56 +0530

    First commit

---
+++ Vuejs/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ResumeParser />
+  </div>
+</template>
+
+<script>
+import ResumeParser from './components/ResumeParser.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ResumeParser
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vuejs/src/components/ResumeParser.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <b-form submit="submit">
+      <div class="container">
+        <h1>Candidate Info</h1>
+        <p>Please fill in this form to apply for a job.</p>
+        <b-row v-for="field in shortcutFields" :key="field.key">
+          <b-col>
+            <label :for="field.key">{{ field.label }}</label>
+          </b-col>
+          <b-col>
+            <b-form-textarea
+              class="input truncate"
+              :id="field.key"
+              v-model="shortcuts[field.key]"
+              :placeholder="field.label"
+              type="text"
+            ></b-form-textarea>
+          </b-col>
+        </b-row>
+      </div>
+      <b-button @click="submit()">Save</b-button>
+    </b-form>
+  </div>
+</template>
+<script>
+import PredefinedShorcuts from "@/js/mixins/import-csv.js";
+export default {
+  name: "ResumeParser",
+  data() {
+    return {
+      shortcuts: [],
+      shortcutFields: [
+        { key: "college_name", label: "College Name" },
+        { key: "company_names", label: "Company Names" },
+        { key: "degree", label: "Degree" },
+        { key: "designation", label: "Designation" },
+        { key: "email", label: "Email" },
+        { key: "experience", label: "Experience" },
+        { key: "name", label: "Name" },
+        { key: "skills", label: "Skills" },
+        { key: "total_experience", label: "Experience" },
+      ],
+    };
+  },
+  mounted() {
+    // Get data Resume Metadata
+    this.shortcuts = PredefinedShorcuts.items[0];
+    this.filterData();
+  },
+  methods: {
+    // filter data to avoid object format data to string
+    filterData() {
+      this.shortcutFields.forEach((field) => {
+        if (typeof this.shortcuts[field.key] == "object") {
+          this.shortcuts[field.key] = this.shortcuts[field.key].join(",");
+        }
+      });
+    },
+    submit(){
+      alert("Successfully saved resume")
+      console.log("Successfully saved")
+    }
+  },
+};
+</script>
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+.container {
+  padding: 16px;
+}
+input,
+textarea {
+  width: 50%;
+  padding: 10px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+}
+</style>
